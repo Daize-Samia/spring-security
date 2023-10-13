7de13623146e58e3233ae631e6224e794f8ad15a
Daize-Samia <daizesamia@gmail.com>
2023-10-13 23:08:51 +0100

    create docker image on local

--- src/main/java/com/ensao/springsecurityproject/repository/TokenRepo.java
+++ src/main/java/com/ensao/springsecurityproject/repository/TokenRepo.java
@@ -2,12 +2,14 @@ package com.ensao.springsecurityproject.repository;
 
 import com.ensao.springsecurityproject.Token.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface TokenRepo extends JpaRepository<Token,Integer> {
+    @Modifying
     @Query("""
     select t from Token t inner join User u on t.user.id = u.id
     where u.id = :userId and (t.expired = false or t.revoked = false)
